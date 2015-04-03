8c8834a3a14483fc2599d1d412d0d0be487f742b
katherine <katherine@katherine-Aspire-E1-531>
2015-04-03 00:13:47 -0400

    ultimo cambios kathe

--- supergestor/gestor/views.py
+++ supergestor/gestor/views.py
@@ -8,7 +8,7 @@ from django.http.response import HttpResponseRedirect
 from django.http import HttpResponse
 from django.core.urlresolvers import reverse
 from django.core.exceptions import ObjectDoesNotExist
-from gestor.models import MyUser, asignacion, proyecto, rol, rol_sistema
+from gestor.models import MyUser, asignacion, proyecto, rol, Flujo
 from django import forms
 from django.core.mail.message import EmailMessage
 from django.template.context import RequestContext
@@ -123,7 +123,7 @@ class FormularioRolProyecto(forms.ModelForm):
     descripcion= forms.CharField()"""
     class Meta:
         model= rol
-        fields=['permisos','nombre','descripcion']
+        fields=['permisos','nombre_rol_id','descripcion']
         
         
     
